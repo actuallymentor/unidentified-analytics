d2230cb03f7ed5f687a497bb06a2b656f1d48119
Mentor <mentor@palokaj.co>
2024-04-03 15:46:22 +0200

    🔥 remove svgr plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react(), svgr()],
-  }
-})
+export default defineConfig( () => {
+    return {
+        build: {
+            outDir: 'build',
+        },
+        plugins: [ react() ],
+    }
+} )
 
