d6587c6e489721ed1287d0d74fb7d6ecaf251f28
Mentor <mentor@palokaj.co>
2024-04-05 16:32:11 +0200

    ✨ migrate over to using only hashed data

--- functions/index.js
+++ functions/index.js
@@ -9,8 +9,12 @@ export const register_touch = v2_onrequest( [ 'max_concurrency' ], _register_tou
 
 // Touch collation
 import { collate_touch_count as _collate_touch_count } from './modules/touch.mjs'
-export const collate_touch_count = onDocumentCreated( 'touches/{touch_id}', _collate_touch_count )
+export const collate_touch_count = onDocumentCreated( 'hashed_touches/{touch_id}', _collate_touch_count )
 
 // Historical stats
 import { get_historical_stats as _get_historical_stats } from './modules/statistics.mjs'
 export const get_historical_stats = v2_oncall( _get_historical_stats )
+
+// Migrate IP data
+// import { migrate_ip_data_to_hashed_data as _migrate_ip_data_to_hashed_data } from './modules/statistics.mjs'
+// export const migrate_ip_data_to_hashed_data = v2_onrequest( [ 'long_timeout' ], _migrate_ip_data_to_hashed_data )
